0a8db91280795066e311c446cfdc279b31a053d7
Xavier Lobez <xavierlobez@hotmail.fr>
2023-10-16 23:20:22 +0200

    Update Card

--- pokemonEvolution/src/App.jsx
+++ pokemonEvolution/src/App.jsx
@@ -18,18 +18,37 @@ const Flambusard = {
 function App() {
   const [count, setCount] = useState(0);
   const currentPokemon = count < 18 ? Passerouge : count < 36 ? Braisillon : Flambusard;
+  const container = {display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '100vh',
+                    border: '2px solid #333',
+                    backgroundImage: 'url("./public/assets/backgroundImage.jpg")',
+                    color: '#fcfdf9',
+                    }
+
+   const imageStyle = {
+    maxWidth: '75%',
+    maxHeight: '75%',
+   }                
+   
+   
 
   return (
     <div>
-      <img src={currentPokemon.image} alt={currentPokemon.firstName} />
-      <h1>{currentPokemon.firstName}</h1>
+      <section style = {container}>
+      <img src={currentPokemon.image} alt={currentPokemon.firstName} style={imageStyle} />
+      <h2>{currentPokemon.firstName}</h2>
       <p>{currentPokemon.firstName}</p>
+      
 
       <div className="card">
         <button onClick={() => setCount(count + 1)}>
           Level {count}
         </button>
       </div>
+      </section>
     </div>
   );
 }
