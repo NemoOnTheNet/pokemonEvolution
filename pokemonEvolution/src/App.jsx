10203914ecaf81ba7972936e9f3afd5408bc55fb
Xavier Lobez <xavierlobez@hotmail.fr>
2023-10-16 14:54:41 +0200

    Loading flight types

--- pokemonEvolution/src/App.jsx
+++ pokemonEvolution/src/App.jsx
@@ -1,28 +1,39 @@
 import { useState } from 'react'
 import './App.css'
 import roucoups from './assets/Roucoups.png'
-import roucool from './assets/250px-Roucool.png'
+import roucool from './assets/Roucool.png'
 import roucarnage from './assets/roucarnage3.png'
+import braisilion from './assets/Braisilion.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
-      <div>
-      </div>
       {count < 18 ? <div>
-        <img src={roucool} alt="" /> <h1>Roucool est votre ami</h1></div> :
+        <img src={roucool} alt="" /> <h1>Roucool est votre ami</h1>
+      </div> :
+
         count < 36 ? <div> <img src={roucoups} alt="" /> <h1>Roucoups est un compagnon fidèle</h1> </div> :
-          <div><img src={roucarnage} alt="" /> <h1>Roucarnage est le maitre des cieux.</h1> </div>}
+      <div><img src={roucarnage} alt="" /> <h1>Roucarnage est le maitre des cieux.</h1> </div>}
+      <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+          level of your pokemon is {count}
+      </button>
+      </div>
+    
+    
+      {count < 18 ? <div>
+        <img src={combée} alt="" /> <h1>Combée ne vous pique pas</h1></div> :
+        count < 36 ? <div> <img src={mothim} alt="" /> <h1>Mothim fidèle à sa reine</h1> </div> :
+          <div><img src={vespicen} alt="" /> <h1>Faites place à la reine Vespicén</h1> </div>}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           level of your pokemon is {count}
         </button>
       </div>
-
-
-    </>
+     </>
   )
 }
 
