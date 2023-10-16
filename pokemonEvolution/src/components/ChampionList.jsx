709d33390d14618b5b54dbfdc4bcb49f10da1919
Robin Fantino <robin.fantino.rf@gmail.com>
2023-10-16 15:17:55 +0200

    Add image of the champions and create a liste of them

---
+++ pokemonEvolution/src/components/ChampionList.jsx
@@ -0,0 +1,24 @@
+const championList = [
+    {
+        id: "Adriane",
+        type: "Feu",
+        town: "Arène de Vermilava",
+    },
+    {
+        id: "Amaro",
+        type: "Plante",
+        town: "Arène de Port Tempères",
+    },
+    {
+        id: "Astera",
+        type: "Psy",
+        town: "Arène de Flusselles",
+    },
+    {
+        id: "Carolina",
+        type: "Vol",
+        town: "Arène de Parsemille",
+    },
+]
+
+export default championList;
