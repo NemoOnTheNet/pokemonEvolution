e0d49ecdd8c018eb275ed688fd3bc9dc15d562a3
Romain <98345704+Romlion@users.noreply.github.com>
2023-10-16 17:16:14 +0200

    I create Pokelist

--- pokemonEvolution/src/App.jsx
+++ pokemonEvolution/src/App.jsx
@@ -7,30 +7,18 @@ import roucoups from './assets/Roucoups.png'
 import roucool from './assets/250px-Roucool.png'
 import roucarnage from './assets/roucarnage3.png'
 
-// pokemons de type feu !
-import Salamèche from './assets/Salamèche.png'
-import Reptincel from './assets/Reptincel.png'
-import Dracaufeu from './assets/Dracaufeu.png'
-import Héricendre from './assets/Héricendre.png'
-import Feurisson from './assets/Feurisson.png'
-import Typhlosion from './assets/Typhlosion.png'
-import Chochodile from './assets/Chochodile.png'
-import Crocogril from './assets/Crocogril.png'
-import Flâmigator from './assets/Flâmigator.png'
-
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   <>
-      
-      {count < 18 ? <div>
+
+      {/* {count < 18 ? <div>
         <img src={roucool} alt="Roucool" /> <h1>Roucool est votre ami</h1></div> :
         count < 36 ? <div> <img src={roucoups} alt="Roucoups" /> <h1>Roucoups est un compagnon fidèle</h1> </div> :
           <div><img src={roucarnage} alt="Roucarnage" /> <h1>Roucarnage est le maitre des cieux.</h1> </div>}
-
+      
       {count < 16 ? <div>
         <img src={Salamèche} alt="Salamèche" /> <h1>Salamèche et loyal</h1></div> :
         count < 36 ? <div> <img src={Reptincel} alt="Reptincel" /> <h1>Reptincel et un taigneux</h1></div> :
@@ -44,7 +32,7 @@ function App() {
       {count < 16 ? <div>
         <img src={Chochodile} alt="Chochodile" /> <h1>Petit croco sympa, on joue ensemble</h1></div> :
         count < 36 ? <div> <img src={Crocogril} alt="Crocogril" /> <h1>Regarde je danse, je suis chaud</h1></div> :
-        <div><img src={Flâmigator} alt="Flâmigator" /><h1>J'ai un oiseau en feu sur le nez, il me sert de micro dans l'une de mes attaques</h1></div>}
+        <div><img src={Flâmigator} alt="Flâmigator" /><h1>J'ai un oiseau en feu sur le nez, il me sert de micro dans l'une de mes attaques</h1></div>} */}
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- pokemonEvolution/src/PokemonList.jsx
+++ pokemonEvolution/src/PokemonList.jsx
@@ -1,11 +1,11 @@
 function PokemonList() {
 
 const PokemonList = [
-    {id: 1, name: 'Salamèche', Image: './assets/Salamèche.png', type: "feu"},
-    {id: 2, name: 'Héricendre', Image: './assets/Héricendre.png', type:"feu"},
-    {id: 3, name:'Chochodile', Image: './assets/Chochodile.png', type:"feu"},
-    {id: 4, name:'Roucool', Image: './assets/250-Roucool.png', type:"vol"},
-    {id: 5, name: , Image: , type:},
+    {id: 1, name: 'Salamèche', Image: './assets/Salamèche.png', type: "feu", level16: "./assets/Reptincel.png", level36: "./assets/Dracaufeu.png"},
+    {id: 2, name: 'Héricendre', Image: './assets/Héricendre.png', type: "feu", level17: "./assets/Feurisson", level36: "./assets/Typhlosion.png"},
+    {id: 3, name:'Chochodile', Image: './assets/Chochodile.png', type: "feu", level16: "./assets/Crocogril", level36: "./assets/Flâmigator.png"},
+    {id: 4, name:'Roucool', Image: './assets/250-Roucool.png', type: "vol", level18: "./assets/Roucoups.png", level36: "./assets/roucarnage3.png"},
+    {id: 5, name: 'bulbizarre', Image:'./assets/bulbizarre.png', type:"plante", level16: "./assets/Herbizarre.png", level},
     {id: 6, name: , Image: , type:},
     {id: 7, name: , Image: , type:},
     {id: 8, name: , Image: , type:},
