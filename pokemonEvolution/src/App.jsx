0ac768a3a01d659ed5d94579f1e8c3639530dba1
Romain <98345704+Romlion@users.noreply.github.com>
2023-10-16 16:33:44 +0200

    LOL

--- pokemonEvolution/src/App.jsx
+++ pokemonEvolution/src/App.jsx
@@ -1,28 +1,59 @@
 import { useState } from 'react'
 import './App.css'
+
+
+// pokemons de type vol !
 import roucoups from './assets/Roucoups.png'
 import roucool from './assets/250px-Roucool.png'
 import roucarnage from './assets/roucarnage3.png'
+
+// pokemons de type feu !
+import Salamèche from './assets/Salamèche.png'
+import Reptincel from './assets/Reptincel.png'
+import Dracaufeu from './assets/Dracaufeu.png'
+import Héricendre from './assets/Héricendre.png'
+import Feurisson from './assets/Feurisson.png'
+import Typhlosion from './assets/Typhlosion.png'
+import Chochodile from './assets/Chochodile.png'
+import Crocogril from './assets/Crocogril.png'
+import Flâmigator from './assets/Flâmigator.png'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      </div>
+  <>
+      
       {count < 18 ? <div>
-        <img src={roucool} alt="" /> <h1>Roucool est votre ami</h1></div> :
-        count < 36 ? <div> <img src={roucoups} alt="" /> <h1>Roucoups est un compagnon fidèle</h1> </div> :
-          <div><img src={roucarnage} alt="" /> <h1>Roucarnage est le maitre des cieux.</h1> </div>}
+        <img src={roucool} alt="Roucool" /> <h1>Roucool est votre ami</h1></div> :
+        count < 36 ? <div> <img src={roucoups} alt="Roucoups" /> <h1>Roucoups est un compagnon fidèle</h1> </div> :
+          <div><img src={roucarnage} alt="Roucarnage" /> <h1>Roucarnage est le maitre des cieux.</h1> </div>}
+
+      {count < 16 ? <div>
+        <img src={Salamèche} alt="Salamèche" /> <h1>Salamèche et loyal</h1></div> :
+        count < 36 ? <div> <img src={Reptincel} alt="Reptincel" /> <h1>Reptincel et un taigneux</h1></div> :
+          <div><img src={Dracaufeu} alt="Dracaufeu" /><h1>Ne venait pas le chercher, il vous carboniserait</h1></div>}
+
+      {count < 17 ? <div>
+        <img src={Héricendre} alt="Héricendre" /> <h1>Je suis timide et j'ai peur</h1></div> :
+        count < 36 ? <div> <img src={Feurisson} alt="Feurisson" /><h1>Mon dresseur et cool avec moi</h1></div> :
+        <div><img src={Typhlosion} alt="Typhlosion" /><h1>C'est moi le plus doué</h1></div>}
+
+      {count < 16 ? <div>
+        <img src={Chochodile} alt="Chochodile" /> <h1>Petit croco sympa, on joue ensemble</h1></div> :
+        count < 36 ? <div> <img src={Crocogril} alt="Crocogril" /> <h1>Regarde je danse, je suis chaud</h1></div> :
+        <div><img src={Flâmigator} alt="Flâmigator" /><h1>J'ai un oiseau en feu sur le nez, il me sert de micro dans l'une de mes attaques</h1></div>}
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           level of your pokemon is {count}
         </button>
       </div>
 
-
-    </>
+      </>
+  
   )
 }
 
---
+++ pokemonEvolution/src/PokemonList.jsx
@@ -0,0 +1,19 @@
+function PokemonList() {
+
+const PokemonList = [
+    {id: 1, name: 'Salamèche', Image: './assets/Salamèche.png', type: "feu"},
+    {id: 2, name: 'Héricendre', Image: './assets/Héricendre.png', type:"feu"},
+    {id: 3, name:'Chochodile', Image: './assets/Chochodile.png', type:"feu"},
+    {id: 4, name:'Roucool', Image: './assets/250-Roucool.png', type:"vol"},
+    {id: 5, name: , Image: , type:},
+    {id: 6, name: , Image: , type:},
+    {id: 7, name: , Image: , type:},
+    {id: 8, name: , Image: , type:},
+    {id: 9, name: , Image: , type:},
+    {id: 10, name: , Image: , type:},
+    {id: 11, name: , Image: , type:},
+    {id: 12, name: , Image: , type:},
+];
+}
+
+export default PokemonList;
