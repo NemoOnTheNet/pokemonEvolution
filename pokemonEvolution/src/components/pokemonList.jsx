d3c3b77dae36c9b6bd9a12eaad7b4399304e2981
Robin Fantino <robin.fantino.rf@gmail.com>
2023-10-16 14:48:37 +0200

    add some champion imgage and the escalidraw for the groupe

--- pokemonEvolution/src/components/pokemonList.jsx
+++ pokemonEvolution/src/components/pokemonList.jsx
@@ -1,20 +1,20 @@
 const pokemonList = [
-    [ 
-    {
-        pokemonName: 'bulbizarre',
-        imageSrc: './assets/bulbizarre.png',
-        evolve: 16,
-    },
-    {
-        pokemonName: 'herbizarre',
-        imageSrc: './'
-    },
-    {
-        pokemonName: 'florizarre',
-        imageSrc: './assets/bulbizarre.png',
-        evolve: 16,
-    }
-]
+    [
+        {
+            pokemonName: 'bulbizarre',
+            imageSrc: './assets/bulbizarre.png',
+            evolve: 16,
+        },
+        {
+            pokemonName: 'herbizarre',
+            imageSrc: './'
+        },
+        {
+            pokemonName: 'florizarre',
+            imageSrc: './assets/bulbizarre.png',
+            evolve: 16,
+        }
+    ]
 
 ]
 
