4fea41c5fc2cd7761274cb7f579e8d45f6373347
Xavier Lobez <xavierlobez@hotmail.fr>
2023-10-16 17:17:12 +0200

    Creation of the card

--- pokemonEvolution/src/App.jsx
+++ pokemonEvolution/src/App.jsx
@@ -1,40 +1,37 @@
-import { useState } from 'react'
-import './App.css'
-import roucoups from './assets/Roucoups.png'
-import roucool from './assets/Roucool.png'
-import roucarnage from './assets/roucarnage3.png'
-import braisilion from './assets/Braisilion.png'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    
-    <>
-      {count < 18 ? <div>
-        <img src={roucool} alt="" /> <h1>Roucool est votre ami</h1>
-      </div> :
-
-        count < 36 ? <div> <img src={roucoups} alt="" /> <h1>Roucoups est un compagnon fidèle</h1> </div> :
-      <div><img src={roucarnage} alt="" /> <h1>Roucarnage est le maitre des cieux.</h1> </div>}
-      <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-          level of your pokemon is {count}
-      </button>
-      </div>
-    
-    
-      {count < 18 ? <div>
-        <img src={combée} alt="" /> <h1>Combée ne vous pique pas</h1></div> :
-        count < 36 ? <div> <img src={mothim} alt="" /> <h1>Mothim fidèle à sa reine</h1> </div> :
-          <div><img src={vespicen} alt="" /> <h1>Faites place à la reine Vespicén</h1> </div>}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          level of your pokemon is {count}
-        </button>
-      </div>
-     </>
-  )
+const Passerouge = {
+  image: "http://localhost:5174/assets/Passerouge.png",
+  firstName: "Passerouge",
 }
 
-export default App
+const Braisillon = {
+  image: "http://localhost:5174/assets/Braisillon.png",
+  firstName: "Braisillon",
+}
+
+const Flambusard = {
+  image: "http://localhost:5174/assets/Flambusard.png",
+  firstName: "Flambusard",
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+  const currentPokemon = count < 18 ? Passerouge : count < 36 ? Braisillon : Flambusard;
+
+  return (
+    <div>
+      <img src={currentPokemon.image} alt={currentPokemon.firstName} />
+      <h1>{currentPokemon.firstName}</h1>
+      <p>{currentPokemon.firstName}</p>
+
+      <div className="card">
+        <button onClick={() => setCount(count + 1)}>
+          Level {count}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
